d9014de4a52d54b2eb695459f85289bc11f5c314
blaskoasky <iriantomauduta@gmail.com>
2021-11-26 22:25:09 +0700

    Initial commit

---
+++ app/src/main/java/com/blaskoasky/iri/myplantdiary/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.blaskoasky.iri.myplantdiary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blaskoasky.iri.myplantdiary.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
