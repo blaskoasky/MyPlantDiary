4205d32d326090e92bfa8b8aa0fbd23a7f13b09a
blaskoasky <iriantomauduta@gmail.com>
2021-11-29 14:19:13 +0700

    add getter setter MainViewModel.kt

--- app/src/main/java/com/blaskoasky/iri/myplantdiary/ui/main/MainViewModel.kt
+++ app/src/main/java/com/blaskoasky/iri/myplantdiary/ui/main/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.blaskoasky.iri.myplantdiary.ui.main
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.blaskoasky.iri.myplantdiary.dto.Plant
@@ -7,7 +8,7 @@ import com.blaskoasky.iri.myplantdiary.service.PlantService
 
 class MainViewModel : ViewModel() {
 
-    var plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
+    private var _plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
     var plantService: PlantService = PlantService()
 
     init {
@@ -17,4 +18,9 @@ class MainViewModel : ViewModel() {
     fun fetchPlants(plantName: String) {
         plants = plantService.fetchPlants(plantName)
     }
+
+    var plants : MutableLiveData<ArrayList<Plant>>
+        get() { return _plants }
+        set(value) { _plants = value }
+
 }
