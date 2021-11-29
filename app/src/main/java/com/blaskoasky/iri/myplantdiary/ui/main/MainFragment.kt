43f1951b3a238d4b8eba920a1ba985aac7bb719d
blaskoasky <iriantomauduta@gmail.com>
2021-11-29 16:40:09 +0700

    add save file photo external sdcard

--- app/src/main/java/com/blaskoasky/iri/myplantdiary/ui/main/MainFragment.kt
+++ app/src/main/java/com/blaskoasky/iri/myplantdiary/ui/main/MainFragment.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
@@ -12,10 +13,14 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.blaskoasky.iri.myplantdiary.R
 import com.blaskoasky.iri.myplantdiary.databinding.MainFragmentBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainFragment : Fragment() {
 
@@ -23,11 +28,13 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
         const val CAMERA_PERMISSION_REQUEST_CODE = 3001
         const val CAMERA_REQUEST_CODE = 3002
-
+        const val SAVE_PHOTO_IMAGE_REQUEST_CODE = 3003
     }
 
     private lateinit var mainFragmentBinding: MainFragmentBinding
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var currentPhotoPath: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -95,9 +102,30 @@ class MainFragment : Fragment() {
 
     private fun takePhoto() {
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(requireContext().packageManager)?.also {
-                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
+            takePictureIntent.resolveActivity(requireContext().packageManager)
+            if (takePictureIntent != null) {
+                val photoFile: File = createImageFile()
+                photoFile?.also {
+                    val photoURI = FileProvider.getUriForFile(
+                        requireActivity().applicationContext,
+                        "com.blaskoasky.iri.myplantdiary",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoFile)
+                    startActivityForResult(takePictureIntent, SAVE_PHOTO_IMAGE_REQUEST_CODE)
+                }
+            } else {
+                Toast.makeText(context, "Unable to save photo", Toast.LENGTH_SHORT).show()
             }
+        }
+    }
+
+    private fun createImageFile(): File {
+        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File? = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+
+        return File.createTempFile("PlantDiary${timestamp}", ".jpg", storageDir).apply {
+            currentPhotoPath = absolutePath
         }
     }
 
