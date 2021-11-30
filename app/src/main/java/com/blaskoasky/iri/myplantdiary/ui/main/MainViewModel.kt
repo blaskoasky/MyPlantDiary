1911947fee3b44a231e61c33b6b8b7d0ff076340
blaskoasky <iriantomauduta@gmail.com>
2021-11-30 15:27:06 +0700

    add save to firebase

--- app/src/main/java/com/blaskoasky/iri/myplantdiary/ui/main/MainViewModel.kt
+++ app/src/main/java/com/blaskoasky/iri/myplantdiary/ui/main/MainViewModel.kt
@@ -1,25 +1,47 @@
 package com.blaskoasky.iri.myplantdiary.ui.main
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.blaskoasky.iri.myplantdiary.dto.Plant
+import com.blaskoasky.iri.myplantdiary.dto.Specimen
 import com.blaskoasky.iri.myplantdiary.service.PlantService
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class MainViewModel : ViewModel() {
 
     private var _plants: MutableLiveData<ArrayList<Plant>> = MutableLiveData<ArrayList<Plant>>()
-    var plantService: PlantService = PlantService()
+    private var plantService: PlantService = PlantService()
+    private lateinit var firestore: FirebaseFirestore
+
 
     init {
         fetchPlants("e")
+        firestore = FirebaseFirestore.getInstance()
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
     }
 
     fun fetchPlants(plantName: String) {
         plants = plantService.fetchPlants(plantName)
     }
 
+    fun save(specimen: Specimen) {
+
+        val document = firestore.collection("specimens").document()
+
+        val set = document.set(specimen)
+        set.addOnSuccessListener {
+            specimen.specimenId = document.id
+            Log.d("Firebase Save", "document Saved")
+        }
+        set.addOnFailureListener {
+            Log.d("Firebase Save", "Save Failed")
+        }
+    }
+
     // this for giving and receiving data to public
-    var plants: MutableLiveData<ArrayList<Plant>>
+    internal var plants: MutableLiveData<ArrayList<Plant>>
         get() {
             return _plants
         }
